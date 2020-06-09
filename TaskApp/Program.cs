86697869100fd03ca671bfb2a9a3732ff9db88d0
13events <deadworlds@me.com>
2020-06-09 12:16:22 -0700

    Set up basic Main method Created Task struct.

---
+++ TaskApp/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+
+namespace TaskApp
+{
+    class MainClass
+    {
+        
+   
+
+        public static void Main(string[] args)
+        {
+            Task taskList = new Task();
+
+            string userName = Environment.UserName;
+            string userSelection = string.Empty;
+
+            PrintWelcome();
+
+            while (!userSelection.Equals('q'))
+            {
+                PrintMenu();
+
+                Console.Write("===> ");
+                userSelection = Console.ReadLine();
+
+                switch (userSelection.ToLower())
+                {
+                    case "1":
+                        Console.WriteLine("Show task list. \n");
+                        break;
+                    case "2":
+                        Console.WriteLine("Add a new task. \n");
+                        break;
+                    case "3":
+                        Console.WriteLine("Mark a task as completed. \n");
+                        break;
+                    case "q":
+                        Console.WriteLine("Quitting application... \n");
+                        break;
+                    default:
+                        return;
+                }
+            }
+            
+            
+        }
+
+        static public void PrintWelcome()
+        {
+            Console.WriteLine("********** Welcome to the TaskApp **********");
+            Console.WriteLine("Make your selection using the menu below.\n");
+        }
+
+        static public void PrintMenu()
+        {
+            Console.WriteLine("1) Show Task List");
+            Console.WriteLine("2) Add a new Task");
+            Console.WriteLine("3) Mark a task as completed");
+        }
+
+        private void AddTask(string taskName)
+        {
+        }
+    }
+}
---
+++ TaskApp/TaskStruct.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace TaskApp
+{
+    public struct Task
+    {
+        private string[] tasks;
+        private string task;
+        private string timeCreated;
+        private string timeDue;
+
+        public Task(string taskName)
+        {
+            tasks = new string[] { };
+            task = taskName;
+            timeCreated = DateTime.Now.Date.ToShortDateString();
+            timeDue = DateTime.Now.Date.ToShortDateString();
+        }
+
+        public Task (string taskName, DateTime dueDate)
+        {
+            tasks = new string[] { };
+            task = taskName;
+            timeCreated = DateTime.Now.Date.ToShortDateString();
+            timeDue = dueDate.ToShortDateString();
+        }
+
+        public void PrintTask()
+        {
+
+        }
+
+    }
+}
